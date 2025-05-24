3b850f16f9295176d49d8017aa98a945bbb15d59
michalrusak <michal1406@onet.pl>
2025-05-24 12:18:15 +0200

    remove react

--- React-Client/src/api/mock/mockUser.ts
+++
@@ -1,7 +0,0 @@
-import { fakerPL as faker } from '@faker-js/faker';
-import { User as UserType } from '@/types';
-
-export const generateUser = (): UserType => ({
-    name: faker.person.fullName(),
-    avatar: faker.image.avatar(),
-});
--- React-Client/src/types/User.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const userSchema = z.object({
-    name: z.string(),
-    avatar: z.string().url(),
-});
-
-export type User = z.infer<typeof userSchema>;
